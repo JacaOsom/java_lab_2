beb7f3cfd6b56b001967a3f0c895d97045b0956d
JacaOsom <jacekfilipski@gmail.com>
2021-12-18 11:07:33 +0100

    Cw.6 listy, set

--- src/Main.java
+++ src/Main.java
@@ -18,13 +18,58 @@ public class Main {
                 d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
                 elementów listy
           */
-
+        //a:
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zadanie 1:");
+        List<String> stringList = new ArrayList<String>();
+        String nazwaZwierzecia;
+        for (int i = 0; i < 5; i++){
+            System.out.print("Podaj nazwę " + (i+1) + " zwierzęcia: ");
+            nazwaZwierzecia = scanner.nextLine();
+            stringList.add(nazwaZwierzecia);
+        }
+        System.out.println("\nOtrzymane zwierzęta:");
+        wystwietlNazwyZwierzat(stringList);
+        //c
+        stringList.remove(4);
+        stringList.remove(3);
+        System.out.println("\nOtrzymane zwierzęta po usunięciu:");
+        wystwietlNazwyZwierzat(stringList);
+        System.out.println("\nOtrzymany rozmiar listy po usunięciu: " + stringList.size());
+        //d
+        Collections.sort(stringList, Collections.reverseOrder());
+        System.out.println("\nSortowanie odwrotne: ");
+        wystwietlNazwyZwierzat(stringList);
 
          /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
                 niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
                  a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
+        System.out.println("\n");
+        System.out.println("zadanie 2:");
+        List<Integer> liczby = new ArrayList<>();
+        int liczba;
+        for (int j = 0; j < 10; j++){
+            System.out.print("Wprowadź liczbę: ");
+            liczba = scanner.nextInt();
+            liczby.add(liczba);
+        }
+        Collections.sort(liczby);
+        System.out.println("Liczby: " + liczby);
+        Set<Integer> liczby2 = new TreeSet<>(liczby);
+        System.out.println("Liczby set: " + liczby2);
 
+        for (Integer numbers : liczby2) {
+            System.out.print(numbers + ", ");
+        }
+
+    }
+    //zadanie 1
+    //b
+    public static void wystwietlNazwyZwierzat(List<String> stringList){
+        for (String nazwa : stringList) {
+            System.out.print(nazwa + ", ");
+        }
     }
 
 }
